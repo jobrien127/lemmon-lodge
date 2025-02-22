4c94e02d42465d8d11a7d9eb6e926fd1c65599e0
Joseph O'Brien <jmobrien127@gmail.com>
2025-02-21 20:24:06 -0700

    update git ignore and use env vars

--- src/components/Contact/Contact.tsx
+++ src/components/Contact/Contact.tsx
@@ -29,8 +29,12 @@ const Contact: React.FC = () => {
 
         setIsSubmitting(true);
 
-        emailjs.send('EMAILJS_SERVICE_ID', 'EMAILJS_TEMPLATE_ID', templateParams, 'EMAILJS_PUBLIC_KEY')
-            .then((response) => {
+        emailjs.send(
+            process.env.REACT_APP_EMAILJS_SERVICE_ID!,
+            process.env.REACT_APP_EMAILJS_TEMPLATE_ID!,
+            templateParams,
+            process.env.REACT_APP_EMAILJS_PUBLIC_KEY!
+        ).then((response) => {
                 console.log('Email sent successfully!', response.status, response.text);
                 alert('Message sent successfully!');
                 setName('');
